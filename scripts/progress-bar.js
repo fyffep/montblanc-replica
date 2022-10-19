84a7403cb3cf30fad8fc6ee34e5654e3efbf05f8
Pete F <fyffep>
2022-10-18 22:49:08 -0400

    Added slideshow and controls for progress bar

--- scripts/progress-bar.js
+++ scripts/progress-bar.js
@@ -1,21 +1,21 @@
-const MAX_STEPS = 10;
-var step = 1;
-function move() {
-  var bar = document.getElementById("progress-bar-fill");
-  step++;
-  if (step > MAX_STEPS)
-    step = 1;
+// const MAX_STEPS = 2;
+// var step = 1;
+// function increaseProgress() {
+//   var bar = document.getElementById("progress-bar-fill");
+//   step++;
+//   if (step > MAX_STEPS)
+//     step = 1;
 
-  idealWidth = (100.0/MAX_STEPS) * step;
-  width = (100.0/MAX_STEPS) * (step - 1);
+//   idealWidth = (100.0/MAX_STEPS) * step;
+//   width = (100.0/MAX_STEPS) * (step - 1);
   
-  var animation = setInterval(() => {
-    if (width > idealWidth) {
-      clearInterval(animation);
-    }
-    else {
-      bar.style.width = width++ + "%";
-    }
-  }, 10);
-}
-move();
+//   var animation = setInterval(() => {
+//     if (width > idealWidth) {
+//       clearInterval(animation);
+//     }
+//     else {
+//       bar.style.width = width++ + "%";
+//     }
+//   }, 10);
+// }
+// move();
--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,30 +1,50 @@
-//// Slideshow: Full credit to W3 Schools https://www.w3schools.com/howto/howto_js_slideshow.asp ////
-
+const MAX_STEPS = 2;
+const INTERVAL = 10;
 var slideIndex = 1;
-showSlides(slideIndex);
+//Update progress bar
+function increaseProgress() {
+  var bar = document.getElementById("progress-bar-fill");
+
+  idealWidth = (100.0/MAX_STEPS) * slideIndex;
+  width = (100.0/MAX_STEPS) * (slideIndex - 1);
+  
+  var animation = setInterval(() => {
+    if (width > idealWidth) {
+      clearInterval(animation);
+    }
+    else {
+      bar.style.width = width++ + "%";
+    }
+  }, INTERVAL);
+}
+increaseProgress();
+
+
+
+
+//// Slideshow: Full credit to W3 Schools https://www.w3schools.com/howto/howto_js_slideshow.asp ////
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("img-slide");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
+  //Make all children of slides invisible
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  //Show target slide
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  //Change number on button
+  var btnPlusSlide = document.getElementById("btn-plus-slide");
+  btnPlusSlide.innerHTML = "« " + slideIndex;
+
+  increaseProgress();
 } 
+showSlides(slideIndex);
